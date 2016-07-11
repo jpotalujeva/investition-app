52cba0214091e11b439eabe3c9aa9c24a0f7b417
Jevgenija Potalujeva <jpotalujeva@gmail.com>
2016-07-11 23:13:28 +0300

    edited view

---
+++ src/AppBundle/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+
+    public function getCompaniesAction()
+    {
+        $company = $this->getDoctrine()
+            ->getRepository('AppBundle:Company')
+            ->findAll();
+
+        return $this->render('@App/Company/index.html.twig', array(
+            'companies' => $company
+        ));
+    }
+
+}
